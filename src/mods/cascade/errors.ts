dcd0f27264a374ef29a1f3cdd04f6a807bd5afb6
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2023-05-29 11:44:35 +0200

    clean

--- src/mods/cascade/errors.ts
+++ src/mods/cascade/errors.ts
@@ -1,5 +1,3 @@
-import { Err } from "@hazae41/result"
-
 /**
  * Error returned from `tryEnqueue`, `tryError`, `tryClose`, `tryTerminate`
  */
@@ -11,33 +9,4 @@ export class ControllerError extends Error {
     return new ControllerError(undefined, { cause })
   }
 
-}
-
-/**
- * Error catched by `start`, `pull`, `write`, `transform`, `flush`, `cancel`, `abort`, `close`
- */
-export class CatchedError extends Error {
-  readonly #class = CatchedError
-  readonly name = this.#class.name
-
-  static from(cause: unknown) {
-    return new CatchedError(undefined, { cause })
-  }
-
-  static fromAndThrow(cause: unknown): never {
-    throw CatchedError.from(cause)
-  }
-
-}
-
-/**
- * `Err` good errors and throw bad ones
- * @param error 
- * @returns `Err(error)` if not `CatchedError` 
- * @throws `error` if `CatchedError` 
- */
-export function rethrow(error: unknown) {
-  if (error instanceof CatchedError)
-    throw error.cause
-  return new Err(error)
 }
