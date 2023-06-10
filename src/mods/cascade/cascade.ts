1e858f5d2d68c692f04366c782790bb83a2d25be
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2023-06-10 16:35:37 +0200

    writable

--- src/mods/cascade/cascade.ts
+++ src/mods/cascade/cascade.ts
@@ -1,4 +1,5 @@
 import { Err, Ok, Result } from "@hazae41/result"
+import { Promiseable } from "libs/promises/promiseable.js"
 import { ControllerError } from "./errors.js"
 
 export interface Enqueueable<T> {
@@ -46,5 +47,47 @@ export function tryTerminate(terminateable: Terminateable): Result<void, Control
     return new Ok(terminateable.terminate())
   } catch (e: unknown) {
     return new Err(ControllerError.from(e))
+  }
+}
+
+export async function runWithWriter<W>(writable: WritableStream<W>, callback: (writer: WritableStreamDefaultWriter<W>) => Promiseable<void>) {
+  const writer = writable.getWriter()
+
+  try {
+    return await callback(writer)
+  } finally {
+    writer.releaseLock()
+  }
+}
+
+export function runWithWriterSync<W>(writable: WritableStream<W>, callback: (writer: WritableStreamDefaultWriter<W>) => void) {
+  const writer = writable.getWriter()
+
+  try {
+    return callback(writer)
+  } finally {
+    writer.releaseLock()
+  }
+}
+
+export class WriteError extends Error {
+  readonly #class = WriteError
+  readonly name = this.#class.name
+
+  static from(cause: unknown) {
+    return new WriteError(undefined, { cause })
+  }
+
+}
+
+export interface Writable<W> {
+  write(chunk?: W): Promise<void>
+}
+
+export async function tryWrite<W>(writer: WritableStreamDefaultWriter<W>, chunk?: W): Promise<Result<void, WriteError>> {
+  try {
+    return new Ok(await writer.write(chunk))
+  } catch (e: unknown) {
+    return new Err(WriteError.from(e))
   }
 }
