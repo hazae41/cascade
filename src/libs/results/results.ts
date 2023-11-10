9ddb183a7ca5644e0dfdae93fa34e43f5f9e1af6
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2023-11-10 23:23:35 +0100

    fix

--- src/libs/results/results.ts
+++ src/libs/results/results.ts
@@ -1,13 +1,17 @@
-import { Err, Ok, Result } from "@hazae41/result"
+import { Result } from "@hazae41/result"
 
 export type Resultable<T, E> = T | Result<T, E>
 
 export namespace Resultable {
 
   export function okOrThrow<T, E>(result: Resultable<T, E>) {
-    if (result instanceof Ok)
+    if (typeof result !== "object")
+      return
+    if (result == null)
+      return
+    if ("isOk" in result && result.isOk())
       return result.get()
-    if (result instanceof Err)
+    if ("isErr" in result && result.isErr())
       throw result.get()
     return result
   }
