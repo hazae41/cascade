a6e4e5ffcba47ee5565c0bba9a8e7a43a0f83c2d
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2024-03-09 20:29:10 +0100

    test

--- src/mods/cascade/index.test.ts
+++ src/mods/cascade/index.test.ts
@@ -1,1 +1,20 @@
-console.log("no tests")
+import { None } from "@hazae41/option"
+import { HalfDuplex } from "./index.js"
+
+const a = new HalfDuplex()
+const b = new HalfDuplex()
+
+a.input.events.on("message", (data) => {
+  console.log(data)
+  return new None()
+})
+
+b.input.events.on("message", (data) => {
+  b.output.enqueue(data)
+  return new None()
+})
+
+a.inner.readable.pipeTo(b.inner.writable)
+b.inner.readable.pipeTo(a.inner.writable)
+
+a.output.enqueue("hello")
